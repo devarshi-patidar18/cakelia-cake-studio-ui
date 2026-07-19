ff19aebb648331e312790af8d28290081681ec92
devarshi-patidar1812 <devarshi.patidar18@gmail.com>
2026-07-20 01:09:05 +0530

    Gallery

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { OrderComponent } from './order/order.component';
+import { GalleryComponent} from './gallery/gallery.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'order', component: OrderComponent },
+  {path: 'gallery', component: GalleryComponent},
   { path: '**', redirectTo: '' }
 ];
